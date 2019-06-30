f1b1baf6f068893bdde4c8f21c46a85485b96cea
Jan Beich <jbeich@FreeBSD.org>
2019-06-30 23:29:44 +0000

    Expand \x1b to support BSD sed

--- test.sh
+++ test.sh
@@ -12,8 +12,8 @@ debug=-d
 # Orange=client, purple=server
 
 rm -f /tmp/waypipe-server.sock /tmp/waypipe-client.sock
-($waypipe -o $debug client 2>&1 | sed 's/.*/\x1b[33m&\x1b[0m/') &
+($waypipe -o $debug client 2>&1 | sed 's/.*/[33m&[0m/') &
 # ssh-to-self; should have a local keypair set up
-(ssh -R /tmp/waypipe-server.sock:/tmp/waypipe-client.sock localhost $waypipe -o $debug server -- $program) 2>&1 | sed 's/.*/\x1b[35m&\x1b[0m/'
+(ssh -R /tmp/waypipe-server.sock:/tmp/waypipe-client.sock localhost $waypipe -o $debug server -- $program) 2>&1 | sed 's/.*/[35m&[0m/'
 kill %1
 rm -f /tmp/waypipe-server.sock /tmp/waypipe-client.sock
